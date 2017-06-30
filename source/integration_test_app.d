ae044ab51b50ac16b051e1059beb9868130b77e4
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-06-30 20:38:13 +0700

    ddoc tested

--- source/integration_test_app.d
+++ source/integration_test_app.d
@@ -14,20 +14,22 @@ void main()
 
     {
         AutocompleteRequest req;
-        req.kind = RequestKind.search;
+        req.kind = RequestKind.doc;
         req.sourceCode = code;
-        req.searchName = "List";
+        req.cursorPosition = 0x11B;
 
         auto ret = w.doRequest(req);
 
         writeln(ret);
+
+        assert(ret.docComments == ["Main function\\n\\nThis function used for integration testing\\n\\nParams:\\n\\nargs = command line arguments\\n\\nReturns:\\n\\nAlways returns zero"]);
     }
 
     {
         AutocompleteRequest req;
         req.kind = RequestKind.autocomplete;
         req.sourceCode = code;
-        req.cursorPosition = 0x58;
+        req.cursorPosition = 0x140;
 
         auto ret = w.doRequest(req);
         writeln(ret);
--- test_file.d
+++ test_file.d
@@ -1,12 +1,32 @@
 module test_file;
 
+/**
+ * Test list
+ *
+ * This list isn't do anything
+ */
 enum List
 {
     First,
     Second
 }
 
-void main()
+/**
+ * Main function
+ *
+ * This function used for integration testing
+ *
+ * Params:
+ *
+ * args = command line arguments
+ *
+ * Returns:
+ *
+ * Always returns zero
+ */
+int main(char[][] args)
 {
-    auto l = List.
+    auto l = List.First;
+
+    return 0;
 }
