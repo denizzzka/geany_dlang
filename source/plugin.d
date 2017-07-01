ef98732a10ed9964fcb52f1735e4eaa44468575e
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-07-01 17:10:42 +0700

    init_keybindings() added

--- source/plugin.d
+++ source/plugin.d
@@ -2,25 +2,64 @@ version(IntegrationTest){} else:
 
 import geany_plugin_d_api;
 import dcd_wrapper;
+import std.experimental.logger;
 
 enum serverStart = "dub run dcd --build=release --config=server";
+private GeanyPlugin* geany_plugin;
 DcdWrapper wrapper;
 
-extern(System):
-
-gboolean initPlugin(GeanyPlugin *plugin, gpointer pdata)
+void init_keybindings()
 {
-    wrapper = new DcdWrapper();
+    import geany_plugin_d_api.keybindings;
+    import geany_plugin_d_api.pluginutils;
+    import gdk.Gdk: GdkModifierType;
+    import gtk.Widget: GtkWidget;
+
+    GeanyKeyGroup* key_group = plugin_set_key_group(geany_plugin, "dlang_keys", 1, null);
+
+    keybindings_set_item(
+            key_group,
+            0,
+            &force_completion,
+            0,
+            cast(GdkModifierType) 0,
+            "exec",
+            "complete",
+            cast(GtkWidget*) null
+        );
+}
+
+extern(System) nothrow:
+
+void force_completion(guint key_id)
+{
+}
+
+gboolean initPlugin(GeanyPlugin *plugin, gpointer pdata) nothrow
+{
+    geany_plugin = plugin;
+
+    try
+        wrapper = new DcdWrapper();
+    catch(Exception e)
+    {
+        //~ fatal(e.msg);
+
+        return false;
+    }
 
     return true;
 }
 
-void cleanupPlugin(GeanyPlugin *plugin, gpointer pdata)
+void cleanupPlugin(GeanyPlugin *plugin, gpointer pdata) nothrow
 {
-    destroy(wrapper);
+    try
+        destroy(wrapper);
+    catch(Exception e){}
+        //~ fatal(e.msg);
 }
 
-void geany_load_module(GeanyPlugin *plugin)
+void geany_load_module(GeanyPlugin *plugin) nothrow
 {
     plugin.funcs._init = &initPlugin;
     plugin.funcs.cleanup = &cleanupPlugin;
@@ -30,6 +69,8 @@ void geany_load_module(GeanyPlugin *plugin)
     plugin.info._version = "0.0.1";
     plugin.info.author = "Denis Feklushkin <denis.feklushkin@gmail.com>";
 
-    if (GEANY_PLUGIN_REGISTER(plugin, 225))
-        return;
+    try
+        GEANY_PLUGIN_REGISTER(plugin, 225);
+    catch(Exception e){}
+        //~ fatal(e.msg);
 }
